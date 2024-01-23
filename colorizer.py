c650c4db465d698c5fd1bb473ab010cfc89a68a0
muyeed15 <muyeed.al.abdullah@gmail.com>
2024-01-23 15:44:33 +0600

    1.0.0

---
+++ colorizer.py
@@ -0,0 +1,62 @@
+# Colorize function
+def colorize_image(image_path):
+    # Modules
+    import numpy
+    import cv2
+    import os
+
+
+    # Config folder
+    if os.path.exists("config") == False:
+        os.makedirs("config")
+    
+    mod_pro = r"model/colorizer/colorization_deploy_v2.prototxt"
+    mod_pts = r"model/colorizer/pts_in_hull.npy"
+    mod_caf = r"model/colorizer/colorization_release_v2.caffemodel"
+
+    # Load the Model
+    net = cv2.dnn.readNetFromCaffe(mod_pro, mod_caf)
+    pts = numpy.load(mod_pts)
+
+    # Load centers for ab channel quantization used for re-balancing.
+    class8 = net.getLayerId("class8_ab")
+    conv8 = net.getLayerId("conv8_313_rh")
+    pts = pts.transpose().reshape(2, 313, 1, 1)
+    net.getLayer(class8).blobs = [pts.astype("float32")]
+    net.getLayer(conv8).blobs = [numpy.full([1, 313], 2.606, dtype="float32")]
+
+    # Load the input image
+    image = cv2.imread(image_path)
+    scaled = image.astype("float32") / 255.0
+    lab = cv2.cvtColor(scaled, cv2.COLOR_BGR2LAB)
+
+    # Resize
+    resized = cv2.resize(lab, (224, 224))
+    
+    # Colorize
+    try:
+        col_data = int(open(r"config/colorize.ini", "r").read())
+        if col_data not in list(range(0, 110, 10)):
+            col_data = 50
+    except:
+        open(r"config/colorize.ini", "w").write(str(50))
+        col_data = 50
+
+    cap_l = cv2.split(resized)[0]
+    cap_l -= int(col_data)
+    net.setInput(cv2.dnn.blobFromImage(cap_l))
+    ab = net.forward()[0, :, :, :].transpose((1, 2, 0))
+    ab = cv2.resize(ab, (image.shape[1], image.shape[0]))
+    cap_l = cv2.split(lab)[0]
+    colorized = numpy.concatenate((cap_l[:, :, numpy.newaxis], ab), axis=2)
+    colorized = cv2.cvtColor(colorized, cv2.COLOR_LAB2BGR)
+    colorized = numpy.clip(colorized, 0, 1)
+    colorized = (255 * colorized).astype("uint8")
+    cv2.waitKey(0)
+
+    # Output
+    file_name, file_extension = os.path.splitext(os.path.split(image_path)[1])
+    output_path = os.path.split(image_path)[0] + "/" + file_name + "_ProPixel_AI_Col" + file_extension
+    cv2.imwrite(output_path, colorized)
+
+    return output_path
---
+++ model/upscaler/Real-ESRGAN_Model_Link.md
@@ -0,0 +1,3 @@
+## Download Models
+- [RealESRGAN_x2.pth](https://drive.google.com/file/d/1pG2S3sYvSaO0V0B8QPOl1RapPHpUGOaV/view?usp=drive_link)
+- [RealESRGAN_x4.pth](https://drive.google.com/file/d/1SGHdZAln4en65_NQeQY9UjchtkEF9f5F/view?usp=drive_link)
